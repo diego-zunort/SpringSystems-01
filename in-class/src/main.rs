fa978f7c5d83eba74c7e8d922c5a529e1497994d
diego-zunort <diegozunort@gmail.com>
2026-01-28 19:41:25 +0000

    module1 chapters

---
+++ in-class/src/main.rs
@@ -0,0 +1,30 @@
+fn double(x:i32) -> i32{ //With functions, you need to specify the data type
+    //return x*2;
+    x*2
+}
+
+fn main() {
+    
+    /*let mut x = 10; //int -> int8,int16,int32,int64,int128
+    x += 10;
+
+    println!("5*2 + 10 = {}",x);*/
+
+
+    /*let mut result : f32 = 0.0; //int
+    let x: i32 = 5; //float
+    result = result + x as f32; //no implicit conversion
+
+    println!("{}",result);*/
+
+
+    /*let mut x:i32 = 5;
+    //x = 1.012; //you cant
+
+    let x:f32 = x as f32 + 1.012;
+    println!("{}",x);*/
+
+    //say_hi(5);
+
+    //println!("Double {} equals to {}",5,double(5));
+}
